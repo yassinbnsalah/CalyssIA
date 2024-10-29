7c8951c95f599713759faa79f86c1f8e8edc285c
hamza khalloufi <149695381+khalloufiHamzaUFO@users.noreply.github.com>
2024-10-29 17:59:19 +0100

    FixPlant

--- PlantApp/models.py
+++ PlantApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Plant(models.Model):
     name = models.CharField(max_length=100)
     scientific_name = models.CharField(max_length=100, blank=True, null=True)
